91881a3a1292f1b89505686f05612deacb807e40
Tyler Hancock <darklime@live.ca>
2017-10-17 23:37:23 -0600

    Updated the base inquisitor impl.

--- src/main/java/com/diluv/inquisitor/Inquisitor.java
+++ src/main/java/com/diluv/inquisitor/Inquisitor.java
@@ -1,5 +1,45 @@
 package com.diluv.inquisitor;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.diluv.inquisitor.engine.IEngine;
+import com.diluv.inquisitor.report.IReport;
+
+/**
+ * A default implementation of the inquisitor scanner. Handles all the basic requirements for a
+ * scanner.
+ *
+ * @author Tyler Hancock (Darkhax)
+ */
 public class Inquisitor implements IInquisitor {
 
+    /**
+     * A list of all malware engines you're scanning for.
+     */
+    private final List<IEngine> engines = new ArrayList<>();
+
+    /**
+     * Adds a new malware engine to the scanner.
+     *
+     * @param engine The engine to add.
+     */
+    public void addEngine (IEngine engine) {
+
+        this.engines.add(engine);
+    }
+
+    @Override
+    public List<IReport> scanFile (File file) {
+
+        final List<IReport> reports = new ArrayList<>();
+
+        for (final IEngine engine : this.engines) {
+
+            reports.addAll(engine.scanFile(file));
+        }
+
+        return reports;
+    }
 }

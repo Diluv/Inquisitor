4b4c80d47d0de8dc4b3195a3710c356b3cb90266
Tyler Hancock <darklime@live.ca>
2017-10-17 21:08:12 -0600

    Added the skeleton for malware reports.

---
+++ src/main/java/com/diluv/inquisitor/report/IReport.java
@@ -0,0 +1,42 @@
+package com.diluv.inquisitor.report;
+
+import java.io.File;
+
+/**
+ * This interface is used to define a report from a malware engine. If a malware engine finds
+ * that a file has malicious contents, they will provide one or more reports for a file.
+ *
+ * @author Tyler Hancock (Darkhax)
+ */
+public interface IReport {
+
+    /**
+     * Gets the name of the engine engine that generated the report.
+     *
+     * @return The name of the malware engine used to generate the report.
+     */
+    String getEngineName ();
+
+    /**
+     * Gets the title of the report. This should be a short summary of the report that is less
+     * than a sentence.
+     *
+     * @return The title of the report.
+     */
+    String getTitle ();
+
+    /**
+     * Gets the full on description of the report. This can be used to provide further details
+     * about any malware that may have been found.
+     *
+     * @return
+     */
+    String getDescription ();
+
+    /**
+     * A reference to the file that was scanned.
+     *
+     * @return The file that was scanned.
+     */
+    File getFile ();
+}

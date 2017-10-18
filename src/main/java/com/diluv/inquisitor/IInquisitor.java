81207dc4c5c51e1fa760eb56bdb975436983a33e
Tyler Hancock <darklime@live.ca>
2017-10-17 23:33:04 -0600

    Updated the inquisitor interface.

--- src/main/java/com/diluv/inquisitor/IInquisitor.java
+++ src/main/java/com/diluv/inquisitor/IInquisitor.java
@@ -1,5 +1,23 @@
 package com.diluv.inquisitor;
 
+import java.io.File;
+import java.util.List;
+
+import com.diluv.inquisitor.report.IReport;
+
+/**
+ * The backing interface for a scanner.
+ *
+ * @author Tyler Hancock (Darkhax)
+ */
 public interface IInquisitor {
 
+    /**
+     * Scans a file, and provides a list of reports. If no reports are generated the list
+     * should be empty.
+     *
+     * @param file The file to scan.
+     * @return A list of reports generated for the file.
+     */
+    List<IReport> scanFile (File file);
 }
